db51eaa8105a35c3091c089af96201c4593c81f1
szopenkrk <karolkochanski@gmail.com>
2022-11-20 21:06:03 +0100

    Add call to api and button to nonvert

--- App.js
+++ App.js
@@ -1,117 +1,123 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
 import React from 'react';
-import type {Node} from 'react';
 import {
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
-  Text,
-  useColorScheme,
   View,
+  TextInput,
+  TouchableOpacity,
+  Text,
 } from 'react-native';
+import { SelectList } from 'react-native-dropdown-select-list'
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+const App = () => {
+  const [numberFrom, onChangeNumberFrom] = React.useState("");
+  const [numberTo, onChangeNumberTo] = React.useState("");
+  const [amount, onChangeAmount] = React.useState("0");
 
-/* $FlowFixMe[missing-local-annot] The type annotation(s) required by Flow's
- * LTI update could not be added via codemod */
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+  const data = [
+    {key:'1', value:'Mobiles', disabled:true},
+    {key:'2', value:'Appliances'},
+    {key:'3', value:'Cameras'},
+    {key:'4', value:'Computers', disabled:true},
+    {key:'5', value:'Vegetables'},
+    {key:'6', value:'Diary Products'},
+    {key:'7', value:'Drinks'},
+  ];
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  const callApi = async (from, to, amount) => {
+    try {
+      const response = await fetch(`https://my.transfergo.com/api/fx-rates?from=${from}&to=${to}&amount=${amount}`);
+      const json = await response.json();
+      console.log(json);
+    }catch (error) {
+      console.error(error);
+    }
+  }
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView>
+      <View tyle={{ height: '100%', backgroundColor: 'powderblue'}}>
+        <View style={styles.row}>
+          <View style={styles.inputWrap}>
+            <SelectList
+              style={styles.input} 
+              setSelected={(val) => onChangeNumberFrom(val)} 
+              data={data} 
+              save="value"
+            />
+          </View>
+          <View style={styles.inputWrap}>
+            <SelectList
+              style={styles.input} 
+              setSelected={(val) => onChangeNumberTo(val)} 
+              data={data} 
+              save="value"
+            />
+          </View>
         </View>
-      </ScrollView>
+        <View style={styles.row, {marginTop: 80}}>
+            <TextInput
+              style={styles.input}
+              onChangeText={onChangeAmount}
+              value={amount}
+              placeholder="0"
+              keyboardType="numeric"
+            />
+        </View>
+        <View>
+        <TouchableOpacity 
+          style={styles.convertButton} 
+          title="Convert" 
+          onPress={()=> callApi('USD', 'PLN', '100')}
+        >
+          <Text style={styles.convertText}>Convert</Text>
+        </TouchableOpacity>
+        </View>
+      </View>
+      
+      
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  inputWrap: {
+    flex: 1,
+    height: 80,
+    borderColor: "#cccccc",
+    borderBottomWidth: 1,
+    marginBottom: 10
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  row: {
+    height: 80,
+    flex: 1,
+    flexDirection: "row"
   },
-  highlight: {
-    fontWeight: '700',
+  convertButton: {
+    backgroundColor:'#1E6738',
+    marginRight:40,
+    marginLeft:40,
+    marginTop:10,
+    paddingTop:10,
+    paddingBottom:10,
+    backgroundColor:'#1E6738',
+    borderWidth: 1,
+    borderColor: '#fff'
   },
+  convertText: {
+    color:'#fff',
+    textAlign:'center',
+    paddingLeft : 10,
+    paddingRight : 10
+  }
+  
 });
 
 export default App;
