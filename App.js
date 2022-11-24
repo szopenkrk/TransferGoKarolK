2fbf6f9f341e9a4957f65c09cd25f168748925a0
szopenkrk <karolkochanski@gmail.com>
2022-11-24 11:42:19 +0100

    Update elements related to structure

--- App.js
+++ App.js
@@ -6,67 +6,77 @@ import {
   TextInput,
   TouchableOpacity,
   Text,
+  Image
 } from 'react-native';
-import { SelectList } from 'react-native-dropdown-select-list';
 import DropDownPicker from 'react-native-dropdown-picker';
 import currencies from './currencies.json';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const App = () => {
-  const [numberFrom, onChangeNumberFrom] = React.useState("");
-  const [numberTo, onChangeNumberTo] = React.useState("");
+  const [numberFrom, setNumberFrom] = React.useState("");
+  const [numberTo, setNumberTo] = React.useState("");
   const [amount, onChangeAmount] = React.useState("0");
-  const [open, setOpen] = useState(false);
-  const curr = currencies.map(a => { return {key: a.cc, value: a.cc}});
+  const [openFrom, setOpenFrom] = useState(false);
+  const [openTo, setOpenTo] = useState(false);
+  const [convertedTo, setConvertedTo] = useState(""); 
+  const [rate, setupRate] = useState("");
+  const curr = currencies.map(a => { return {label: a.cc, value: a.cc}});
+  const [items, setItems] = useState(curr);
+  const [visibility, setVisibility] = useState(true);
 
   const callApi = async (from, to, amount) => {
+    if(!numberFrom || !numberTo) {
+      alert('Set values to convert')
+      return true
+    }
+    setVisibility(false);
     try {
       const response = await fetch(`https://my.transfergo.com/api/fx-rates?from=${from}&to=${to}&amount=${amount}`);
       const json = await response.json();
       console.log(json);
+      setConvertedTo(json.toAmount);
+      setupRate(json.rate);
     }catch (error) {
       console.error(error);
     }
   }
-  console.log('ASD', currencies.map(a => { return {key: a.cc, value: a.cc}}));
-  const [items, setItems] = useState(curr);
   return (
     <>
       <SafeAreaView>
-        <View tyle={{ height: '100%',}}>
+        <View >
           <View style={styles.row}>
             <View style={styles.inputWrap}>
-              <SelectList
-                style={styles.input, {zIndex: 100}} 
-                maxHeight='200'
-                data={currencies.map(a => { return {key: a.cc, value: a.cc}})}
-                dropDownDirection="TOP"
-                theme="LIGHT"
-                listMode="SCROLLVIEW"
-                onSelect={(selectedItem, index) => {
-                  console.log(selectedItem, index)
-                }}
-                buttonTextAfterSelection={(selectedItem, index) => {
-                  return selectedItem
-                }}
-                rowTextForSelection={(item, index) => {
-                  return item
-                }}
+            <DropDownPicker
+                placeholder="Select"
+                open={openFrom}
+                value={numberFrom}
+                items={items}
+                listMode="FLATLIST"
+                setValue={setNumberFrom}
+                setOpen={setOpenFrom}
+                zIndex={200}
               />
             </View>
-            <Icon.Button name="swap_horiz" backgroundColor="#3b5998"></Icon.Button>
+            <View style={styles.swapbuttonrow}>
+            <Icon.Button name="swap-horiz" size={30} color="#00BFFF" backgroundColor="#ffffff"/>
+            </View>
             <View style={styles.inputWrap}>
-            <DropDownPicker
-              open={open}
-              items={items}
-              theme="DARK"
-              listMode="FLATLIST"
-              setItems={setItems}
-              setOpen={setOpen}
-            />
+              <DropDownPicker
+                placeholder="Select"
+                open={openTo}
+                value={numberTo}
+                items={items}
+                listMode="FLATLIST"
+                setValue={setNumberTo}
+                setOpen={setOpenTo}
+                zIndex={200}
+              />
             </View>
           </View>
-          <View style={styles.row, {marginTop: 80}}>
+          {/* Hidden element after search data */}
+          { visibility &&
+          <>
+          <View style={styles.row, {marginTop: 80, zIndex: -1}}>
               <TextInput
                 style={styles.input}
                 onChangeText={onChangeAmount}
@@ -75,15 +85,46 @@ const App = () => {
                 keyboardType="numeric"
               />
           </View>
-          <View>
-          <TouchableOpacity 
-            style={styles.convertButton} 
-            title="Convert" 
-            onPress={()=> callApi('USD', 'PLN', '100')}
-          >
-            <Text style={styles.convertText}>Convert</Text>
-          </TouchableOpacity>
+          <View style={{zIndex: -1}}>
+            <TouchableOpacity 
+              zIndex="0"
+              style={styles.convertButton} 
+              title="Convert" 
+              onPress={()=> callApi(numberFrom, numberTo, amount)}
+            >
+              <Text style={styles.convertText}>Convert</Text>
+            </TouchableOpacity>
           </View>
+          </>
+          }
+          {/* Results */}
+          { rate && 
+          <>
+            <View style={styles.row, {marginTop: 70}}>
+              <Text style={{width: '80%', padding: 10}}>{amount}{numberFrom} = {rate} {numberTo}</Text>
+            </View>
+            <View style={styles.row}>
+              <View style={styles.inputWrap}>
+              <TextInput
+                  style={styles.input, {zIndex: -1} }
+                  value={amount}
+                  keyboardType="numeric"
+                />
+
+              </View>
+              <View style={styles.inputWrap}>
+              <TextInput
+                  style={styles.input, {zIndex: -1}}
+                  value={convertedTo}
+                />
+              </View>
+            </View>
+           
+            {/* <View>
+              <Text>All figures are live mid-market rates, which are for information purpose only. To see the rates from money transfer, please select sending money option. </Text>
+            </View> */}
+          </>
+          }
         </View>
       </SafeAreaView>
     </>
@@ -91,7 +132,11 @@ const App = () => {
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    padding: 24,
+    backgroundColor: "#eaeaea"
+  },
   input: {
     height: 40,
     margin: 12,
@@ -102,14 +147,18 @@ const styles = StyleSheet.create({
     margin: 10,
     flex: 1,
     height: 80,
-    borderColor: "#cccccc",
-    borderBottomWidth: 1,
     marginBottom: 10
   },
   row: {
     height: 80,
     flex: 1,
     flexDirection: "row"
+  },
+  swapbuttonrow:{
+    height: 50,
+    textAlign: 'center',
+    marginTop: 10,
+    backgroundColor: 'white'
   },
   convertButton: {
     backgroundColor:'#32CD32',
@@ -119,13 +168,19 @@ const styles = StyleSheet.create({
     paddingTop:10,
     paddingBottom:10,
     borderWidth: 1,
-    borderColor: '#fff'
+    borderColor: '#fff',
+    zIndex: -1
   },
   convertText: {
     color:'#fff',
     textAlign:'center',
     paddingLeft : 10,
     paddingRight : 10
+  },
+  iconStyle: {
+    width: '100%', 
+    height: 200,
+    resizeMode : 'contain' 
   }
   
 });
